2da94f3e143af77ea494604ff6a3d303964d611e
haibm <haixm2003@gmail.com>
2024-11-06 10:43:29 +0700

    Revert "Return base project"
    This reverts commit 25eb692b01d53549ee4ed19e66b73634ac274045.

---
+++ DigitalResourcesStore.Entities/ReferralProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalResourcesStore.EntityFramework.Models;
+
+[Table("ReferralProgram")]
+public partial class ReferralProgram
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Column("ReferrerID")]
+    public int ReferrerId { get; set; }
+
+    [Column("ReferredID")]
+    public int ReferredId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime ReferralDate { get; set; }
+
+    public string? Conditions { get; set; }
+
+    [StringLength(255)]
+    public string? Reward { get; set; }
+
+    public bool? IsSuccessful { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CompletionDate { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? ExpiryDate { get; set; }
+
+    [ForeignKey("ReferredId")]
+    [InverseProperty("ReferralProgramReferreds")]
+    public virtual User Referred { get; set; } = null!;
+
+    [ForeignKey("ReferrerId")]
+    [InverseProperty("ReferralProgramReferrers")]
+    public virtual User Referrer { get; set; } = null!;
+}
---
+++ DigitalResourcesStore.Entities/Voucher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalResourcesStore.EntityFramework.Models;
+
+[Table("Voucher")]
+public partial class Voucher
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [StringLength(255)]
+    public string Name { get; set; } = null!;
+
+    [Column(TypeName = "decimal(10, 2)")]
+    public decimal Discount { get; set; }
+
+    public int Type { get; set; }
+
+    public int RemainUsed { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CreatedAt { get; set; }
+
+    [StringLength(255)]
+    public string? CreatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? UpdatedAt { get; set; }
+
+    [StringLength(255)]
+    public string? UpdatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? DeletedAt { get; set; }
+
+    [Column("isDelete")]
+    public bool? IsDelete { get; set; }
+
+    [StringLength(255)]
+    public string? DeletedBy { get; set; }
+
+    [InverseProperty("Voucher")]
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
