296062f3a7ce8d7cde6b18545850665a2353ce3c
haibm <haixm2003@gmail.com>
2024-11-06 10:37:17 +0700

    Revert "Merge branch 'master' of https://github.com/VPGreenLight/DigitalResourcesStore"
    This reverts commit 54aa7d347620d78cd53de413d882cb2934da0c05, reversing
    changes made to 0722ecf60db10185eccfe244943ba15502b0b09b.

--- DigitalResourcesStore.EntityFramework/_Imports.cs
+++ DigitalResourcesStore.EntityFramework/_Imports.cs
@@ -1,5 +1,3 @@
-﻿global using DigitalResourcesStore.Entities;
-global using DigitalResourcesStore.Models;
-global using Microsoft.AspNetCore.Identity;
+﻿global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore;
